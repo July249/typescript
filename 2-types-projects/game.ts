e810ad4ff52dca943b1b94762e101568c5598965
jihyunglee <jhlee7125@gmail.com>
2023-01-25 15:10:55 +0900

    23-01-25 / 2-2 game.ts 과제 제출

--- 2-types-projects/game.ts
+++ 2-types-projects/game.ts
@@ -1,12 +1,41 @@
 /**
  * Let's make a game 🕹
  */
+type Position = {
+  x: number;
+  y: number;
+};
+
+const position: Position = { x: 0, y: 0 };
+
+type Direction = "up" | "down" | "left" | "right";
+
+function move(direction: Direction): Object {
+  switch (direction) {
+    case "up":
+      position.y += 1;
+      return position;
+    case "down":
+      position.y -= 1;
+      return position;
+    case "left":
+      position.x -= 1;
+      return position;
+    case "right":
+      position.x += 1;
+      return position;
+    default:
+      console.log("Incorrect Direction");
+      return position;
+  }
+}
+
 console.log(position); // { x: 0, y: 0}
-move('up');
+move("up");
 console.log(position); // { x: 0, y: 1}
-move('down');
+move("down");
 console.log(position); // { x: 0, y: 0}
-move('left');
+move("left");
 console.log(position); // { x: -1, y: 0}
-move('right');
+move("right");
 console.log(position); // { x: 0, y: 0}
