e604d25131063906a5ba559ab8c528e218ea5a10
jihyunglee <jhlee7125@gmail.com>
2023-01-25 22:48:22 +0900

    23-01-25 / 3-2 class.ts 피드백

--- 3-OOP/3-2-class.ts
+++ 3-OOP/3-2-class.ts
@@ -6,24 +6,22 @@
   };
 
   class CoffeeMachine {
-    #shots: number;
-    #coffeeBeans: number;
-    #BEANS_GRAM_PER_SHOT: number;
-    constructor(
-      shots: number,
-      coffeeBeans: number,
-      BEANS_GRAM_PER_SHOT: number
-    ) {
-      this.#shots = shots;
-      this.#BEANS_GRAM_PER_SHOT = BEANS_GRAM_PER_SHOT;
-      this.#coffeeBeans = coffeeBeans * this.#BEANS_GRAM_PER_SHOT;
+    static BEANS_GRAM_PER_SHOT: number = 7; // class level
+    coffeeBeans: number = 0; // instance (object) level
+
+    constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
     }
 
     makeCoffee(shots: number): CoffeeCup {
-      if (this.#coffeeBeans < this.#shots * this.#BEANS_GRAM_PER_SHOT) {
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAM_PER_SHOT) {
         throw new Error("Not enough coffee beans!");
       }
-      this.#coffeeBeans -= this.#shots * this.#BEANS_GRAM_PER_SHOT;
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAM_PER_SHOT;
       return {
         shots,
         hasMilk: false,
@@ -31,8 +29,14 @@
     }
   }
 
-  const espressoMachine = new CoffeeMachine(2, 3, 7);
+  const espressoMachine = new CoffeeMachine(32);
+  console.log(espressoMachine);
+  const espressoMachine2 = new CoffeeMachine(14);
+  console.log(espressoMachine2);
 
   const coffee = espressoMachine.makeCoffee(2);
   console.log(coffee);
+
+  const espressoMachine3 = CoffeeMachine.makeMachine(3);
+  console.log(espressoMachine3);
 }
