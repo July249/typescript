d7fc1598f0ad72bc2fb26830eff6167c943c3c82
jihyunglee <jhlee7125@gmail.com>
2023-03-14 05:45:06 +0900

    23-03-14 / Make CaffeLatteMachine with inheritance

--- 3-OOP/3-6-inheritance.ts
+++ 3-OOP/3-6-inheritance.ts
@@ -9,64 +9,17 @@
     makeCoffee(shots: number): CoffeeCup;
   }
 
-  class CaffeLatteMachine {
-    private static BEANS_GRAM_PER_SHOT: number = 7;
-    private coffeeBeans: number = 0;
-    private constructor(coffeeBeans: number) {
-      this.coffeeBeans = coffeeBeans;
-    }
-
-    static makeMachine(coffeeBeans: number): CaffeLatteMachine {
-      return new CaffeLatteMachine(coffeeBeans);
-    }
-
-    fillCoffeeBeans(beans: number) {
-      if (beans < 0) {
-        throw new Error('value for beans should be greater than 0');
-      }
-      this.coffeeBeans += beans;
-    }
-
-    clean(): void {
-      console.log('Cleaning the machine ... 🧼');
-    }
-
-    private grindBeans(shots: number) {
-      console.log(`grinding beans for ${shots}`);
-      if (this.coffeeBeans < shots * CaffeLatteMachine.BEANS_GRAM_PER_SHOT) {
-        throw new Error('Not enough coffee beans!');
-      }
-      this.coffeeBeans -= shots * CaffeLatteMachine.BEANS_GRAM_PER_SHOT;
-    }
-
-    private preHeat(): void {
-      console.log('Heating up... 🔥');
-    }
-
-    private extract(shots: number): CoffeeCup {
-      console.log(`Pulling ${shots} shots ... ☕️`);
-
-      return {
-        shots,
-        hasMilk: false,
-      };
-    }
-
-    makeCoffee(shots: number): CoffeeCup {
-      this.grindBeans(shots);
-      this.preHeat();
-      const coffee = this.extract(shots);
-      return { ...coffee, hasMilk: true };
-    }
-  }
-
   class CoffeeMachine implements CoffeeMaker {
     private static BEANS_GRAM_PER_SHOT: number = 7;
     private coffeeBeans: number = 0;
-
-    private constructor(coffeeBeans: number) {
+    // 1. 상속을 확인하기 위해 임시로 public으로 설정한다.
+    public constructor(coffeeBeans: number) {
       this.coffeeBeans = coffeeBeans;
     }
+    // 2. 상속에서 접근이 가능하도록 하기 위해 private에서 protected로 변경한다.
+    // protected constructor(coffeeBeans: number) {
+    //   this.coffeeBeans = coffeeBeans;
+    // }
 
     static makeMachine(coffeeBeans: number): CoffeeMachine {
       return new CoffeeMachine(coffeeBeans);
@@ -111,4 +64,15 @@
       return this.extract(shots);
     }
   }
+
+  class CaffeLatteMachine extends CoffeeMachine {}
+
+  const machine = new CaffeLatteMachine(23);
+  console.log(machine); // CaffeLatteMachine { coffeeBeans: 23 }
+  machine.makeCoffee(2);
+  // grinding beans for 2
+  // Heating up... 🔥
+  // Pulling 2 shots ... ☕️
+  machine.fillCoffeeBeans(23);
+  machine.clean(); // Cleaning the machine ... 🧼
 }
