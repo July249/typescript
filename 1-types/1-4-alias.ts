67bb316efa40c5114a4ba25fd67d456d89e9224b
jihyunglee <jhlee7125@gmail.com>
2023-03-14 03:17:07 +0900

    23-03-14 / Type Alias

--- 1-types/1-4-alias.ts
+++ 1-types/1-4-alias.ts
@@ -1,10 +1,10 @@
 {
   /**
-   * Type Aliases (새로운 타입을 내가 정의할 수 있)
+   * Type Aliases (새로운 타입을 내가 정의할 수 있음)
    */
   type Text = string;
-  const name: Text = "tom";
-  const address: Text = "Korea";
+  const name: Text = 'tom';
+  const address: Text = 'Korea';
   type Num = number;
 
   type Student = {
@@ -13,17 +13,21 @@
   };
 
   const student: Student = {
-    name: "tomas",
+    name: 'tomas',
     age: 12,
   };
 
   /**
    * String Literal Types
    */
-  type Name = "name";
+  type Name = 'name';
   let tomName: Name;
-  tomName = "name";
+  tomName = 'name';
 
-  type JSON = "json";
-  const json: JSON = "json";
+  type JSON = 'json';
+  const json: JSON = 'json'; // 할당할 수 있는 값을 fix할 수 있음
+
+  type Bool = true;
+  // let isCat: Bool = false; // Type 'false' is not assignable to type 'true'
+  let isCat: Bool = true;
 }
